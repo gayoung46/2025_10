854fd8759e24fe0ed5dca01fef0ea57dd2b5c197
gayoung46 <gayoung04066@naver.com>
2025-11-06 15:30:37 +0900

    10_3

--- main.c
+++ main.c
@@ -3,18 +3,13 @@
 
 int main(int argc, char *argv[])
 {
-    int i=0;
-    char str[4];
-    str[0] = 'a';
-    str[1] = 'b';
-    str[2] = 'c';
-    str[3] = '\0';
+    char scr[] = "The worst things to eat before you sleep";
+    char dst[100];
     
-    while( str[i] != '\0')
-    {
-           printf("%c", str[i]);
-           i++;
-    }
+    strcpy(dst,scr);
+    
+    printf("copied string: %s\n", dst);
+    
     
   system("PAUSE");	
   return 0;
